a3b662d4f77da1f0b21046aa85d507ecd752bbaa
davidfrp <davidfrp@outlook.dk>
2021-08-30 20:30:17 +0200

    Added model package.

---
+++ src/main/java/io/davidfrp/aarstiderne/model/Product.java
@@ -0,0 +1,24 @@
+package io.davidfrp.aarstiderne.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @Column(name = "id", updatable = false, nullable = false)
+    private long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "price", nullable = false)
+    private int price;
+}
