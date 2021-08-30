5c19eb56426607b9391c9295705ef21289e6844f
davidfrp <davidfrp@outlook.dk>
2021-08-30 20:09:56 +0200

    Initial commit

---
+++ src/main/java/io/davidfrp/aarstiderne/AarstiderneApplication.java
@@ -0,0 +1,12 @@
+package io.davidfrp.aarstiderne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AarstiderneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AarstiderneApplication.class, args);
+    }
+}
---
+++ src/test/java/io/davidfrp/aarstiderne/AarstiderneApplicationTests.java
@@ -0,0 +1,12 @@
+package io.davidfrp.aarstiderne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AarstiderneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
