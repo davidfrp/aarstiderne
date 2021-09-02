7036ed71e542e2a4522b4fb545ce5a2d294d5311
davidfrp <davidfrp@outlook.dk>
2021-09-02 16:08:53 +0200

    Updated AarstiderneApplication.java

---
+++ src/main/java/com/davidfrp/aarstiderne/AarstiderneApplication.java
@@ -0,0 +1,12 @@
+package com.davidfrp.aarstiderne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AarstiderneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(com.davidfrp.aarstiderne.AarstiderneApplication.class, args);
+    }
+}
